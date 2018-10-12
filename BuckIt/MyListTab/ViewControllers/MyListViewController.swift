9425c55fab9c8dff076139f2703ac5acf09a82c8
Eric Developer <ea33sports@me.com>
2018-10-11 21:18:23 -0600

    UI First Draft

---
+++ BuckIt/MyListTab/ViewControllers/MyListViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MyListViewController.swift
+//  BuckIt
+//
+//  Created by Eric Andersen on 10/10/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+import UIKit
+
+class MyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let button = UIButton(type: UIButton.ButtonType.custom)
+//        button.setImage(UIImage(named: "settings.png"), for: UIControl.State.normal)
+//        button.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+//        let barButton = UIBarButtonItem(customView: button)
+//        self.navigationItem.rightBarButtonItems = [barButton]
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
