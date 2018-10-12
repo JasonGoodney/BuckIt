0d966b9adb3d80cea2aca213cc3e4ae68dd0380f
michael guatambu davis <guatambu@dundak.com>
2018-10-12 14:37:34 -0700

    fixes convenience init? models

---
+++ BuckIt/Sources/IBDesignables.swift
@@ -0,0 +1,9 @@
+//
+//  IBDesignables.swift
+//  BuckIt
+//
+//  Created by Kelly Johnson on 10/12/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+import Foundation
---
+++ BuckIt/Sources/UserCreateAccountViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserCreateAccountViewController.swift
+//  BuckIt
+//
+//  Created by Kelly Johnson on 10/12/18.
+//  Copyright © 2018 Jason Goodney. All rights reserved.
+//
+
+import UIKit
+
+class UserCreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
